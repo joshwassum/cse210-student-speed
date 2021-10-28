4b6cff189eec25493dd3b64acb28b5a7f368f9be
Joshua Wassum <josh.wassum@transformativems.com>
2021-10-27 19:48:19 -0600

    Added comments outlining code tasking

--- speed/game/score.py
+++ speed/game/score.py
@@ -12,8 +12,17 @@ class Score(Actor):
         _points (integer): The number of points the food is worth.
     """
 
+    # Larry
     def __init__(self):
+        # Need to call superclass
+        # call all attributes
+        # set postion equal to 1,0 using the Point class
+        # self.set_postion(position)
+        # self.set_text(as shown in requirements)
         pass
 
+    # Vanessa
     def add_points(self, points):
+        # self._points += points
+        # reset self.set_text(as shown in requirements)
         pass
